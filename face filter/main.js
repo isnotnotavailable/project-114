368b09052f4ac6955a8bcf339aa4b9902448f1ad
isnotnotavailable <87622374+isnotnotavailable@users.noreply.github.com>
2023-02-19 15:15:04 -0500

    Add files via upload

---
+++ face filter/main.js
@@ -0,0 +1,17 @@
+function Setup() {
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+
+function Clear_canvas() {
+    background("white");
+}
+
+function preload(){
+
+}
+
+function draw(){
+
+}
